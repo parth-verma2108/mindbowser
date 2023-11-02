db6985339a6433077421e215834ce2e2e7832e6c
Parth Verma <113232728+parth-verma2108@users.noreply.github.com>
2023-11-02 23:31:27 +0530

    Code refactor

--- discount_calculator.rb
+++ discount_calculator.rb
@@ -1,67 +1,68 @@
 class DiscountCalculator
-	def discount_calculator
-		@items = {
-		  :milk => 3.97,
-		  :bread => 2.17,
-		  :banana => 0.99,
-		  :apple => 0.89
-		}
+  def initialize
+    @items = {
+      milk: 3.97,
+      bread: 2.17,
+      banana: 0.99,
+      apple: 0.89
+    }
 
-		@sale_items = { 
-			:milk => { :quantity => 2, :amount => 5.00 },
-			:bread => { :quantity => 3, :amount => 6.00 }
-		}
+    @sale_items = { 
+      milk: { quantity: 2, amount: 5.00 },
+      bread: { quantity: 3, amount: 6.00 }
+    }
+  end
 
-		discount_prices = Hash.new
-		actual_prices = Hash.new
+  def calculate_discount
+    discount_prices = {}
+    actual_prices = {}
 
-		puts "Please enter all the items purchased separated by a comma"
-		item_names = gets.chomp
-		return if item_names.empty?
-		
-		cart_items = item_names.split(',').map(&:strip).map(&:downcase)
-		
-		cart_items.uniq.each do |item|
-		discount_prices[item] = discount_price(item.to_sym, cart_items.count(item))
-		actual_prices[item] = actual_price(item.to_sym, cart_items.count(item))
-		end
+    puts "Please enter all the items purchased separated by a comma"
+    item_names = gets.chomp
+    return if item_names.empty?
 
-		puts ''
-		puts 'Item     Quantity      Price'
-		puts '----------------------------'
-		discount_prices.keys.each do |item|
-			puts "#{item}      #{cart_items.count(item)}            $#{discount_prices[item]}"
-		end
-		puts ''
-		puts "Total price : $#{discount_prices.values.sum}"
-		puts "You saved $#{sprintf("%.2f", actual_prices.values.sum - discount_prices.values.sum)} today."
-	end
+    cart_items = item_names.split(',').map(&:strip).map(&:downcase)
 
-	def discount_price(item, quantity)
-		total_price = 0
+    cart_items.uniq.each do |item|
+      discount_prices[item] = calculate_item_discount(item, cart_items.count(item))
+      actual_prices[item] = calculate_item_price(item, cart_items.count(item))
+    end
 
-		if @sale_items.keys.include?(item)
-			sale_quantity = @sale_items[item][:quantity]
-			sale_price = (quantity / sale_quantity) * @sale_items[item][:amount]
-			remaining_price = (quantity % sale_quantity) * @items[item]
-			total_price = remaining_price + sale_price
-		elsif @items.keys.include?(item)
-			total_price = @items[item] * quantity
-		end
+    display_cart_summary(discount_prices, cart_items)
+    total_discount = actual_prices.values.sum - discount_prices.values.sum
+    puts "You saved $#{sprintf('%.2f', total_discount)} today."
+  end
 
-		total_price
-	end
+  private
 
-	def actual_price(item, quantity)
-		total_price = 0
+  def calculate_item_discount(item, quantity)
+    if @sale_items.key?(item)
+      sale_quantity = @sale_items[item][:quantity]
+      sale_price = (quantity / sale_quantity) * @sale_items[item][:amount]
+      remaining_price = (quantity % sale_quantity) * @items[item]
+      remaining_price + sale_price
+    elsif @items.key?(item)
+      @items[item] * quantity
+    else
+      0
+    end
+  end
 
-		if @items.keys.include?(item)
-			total_price = @items[item] * quantity
-		end
+  def calculate_item_price(item, quantity)
+    @items.key?(item) ? @items[item] * quantity : 0
+  end
 
-		return total_price
-	end
+  def display_cart_summary(discount_prices, cart_items)
+    puts ''
+    puts 'Item     Quantity      Price'
+    puts '----------------------------'
+    discount_prices.keys.each do |item|
+      puts "#{item}      #{cart_items.count(item)}            $#{sprintf('%.2f', discount_prices[item])}"
+    end
+    puts ''
+    puts "Total price : $#{sprintf('%.2f', discount_prices.values.sum)}"
+  end
 end
 
 discount_calculator = DiscountCalculator.new
-calculate = discount_calculator.discount_calculator
+discount_calculator.calculate_discount
